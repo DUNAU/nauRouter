9ba305877a140b37de9ddd09c0d13afb6916d0c7
OlehMell <oleh.melnechyk@gmail.com>
2019-08-11 13:48:45 +0300

    Add list place of student city

--- src/pages/city.js
+++ src/pages/city.js
@@ -5,9 +5,9 @@ const CityPlace = (props) => {
   const { list } = props;
   return <div>
     <h4>{list.name}</h4>
-    <a href={`${list.location}`} target='_blanck'>{list.office}</a>
+    <a href={`${list.location}`}>{list.office}</a>
     <p>{list.director && `Староста: ${list.director}`}</p>
-    <a href={`https://t.me/${list.contact}`} target='_blanck'>{list.contact}</a>
+    <a href={`https://t.me/${list.contact}`}>{list.contact}</a>
   </div>
 }
 
