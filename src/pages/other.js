bfb2f69abe749e550a6d42a2d1db3038c9bf985f
OlehMell <oleh.melnechyk@gmail.com>
2019-08-21 21:51:25 +0300

    Add new data

--- src/pages/other.js
+++ src/pages/other.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const OtherPlace = (props) => {
   const { list } = props;
-  return <div class="institute-card">
-    <a class="title" href={`${list.location}`}>{`${list.office} ${list.name}`}</a>
-    <p class="name">{list.director && `Контактна особа:: ${list.director}`}</p>
-    <a class="number" href={`tel:${list.phone}`}>{list.phone}</a>
+  return <div className="institute-card">
+    <a className="title" href={`${list.location}`}>{`${list.office} ${list.name}`}</a>
+    <p className="name">{list.director && `Контактна особа: ${list.director}`}</p>
+    <a className="number" href={`tel:${list.phone}`}>{list.phone}</a>
   </div>
 }
 
 export const Other = (list) => {
   console.log(list);
   return <>
-    <p class="header-text">Інші місця</p>
-    <div class="cards">
+    <p className="header-text">Інші місця</p>
+    <div className="cards">
       {list.map((x,id) => <OtherPlace key={id} list={x} />)}
     </div>
   </>;
